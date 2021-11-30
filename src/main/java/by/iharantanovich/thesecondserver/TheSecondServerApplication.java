23bd5da15134dfb14a394e14f0956b6756bd1a3d
garikAntonovich <garik.antonovich@gmail.com>
2021-11-30 11:33:02 +0300

    init project

---
+++ src/main/java/by/iharantanovich/thesecondserver/TheSecondServerApplication.java
@@ -0,0 +1,13 @@
+package by.iharantanovich.thesecondserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSecondServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheSecondServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/iharantanovich/thesecondserver/TheSecondServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.iharantanovich.thesecondserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSecondServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
